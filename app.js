55c1d2552294ee19fcd2c756ad741b66e5a81cdd
pedrormx <pedrormx56@gmail.com>
2020-10-23 23:17:42 -0300

    musicaapi

---
+++ app.js
@@ -0,0 +1,7 @@
+async function letraAPI(){
+	const cantor = document.getElementById("cantor").value
+	const musica = document.getElementById("musica").value
+	const response = await fetch("https://api.vagalume.com.br/search.php?apikey=660a4395f992ff67786584e238f501aa&art="+cantor+"&mus="+musica)
+	const json = await response.json()
+	document.getElementById("letra").innerHTML = json.mus[0].text
+}
