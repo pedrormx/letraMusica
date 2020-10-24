79c55be9e9693768c6c1db6d12211a573d07cca4
pedrormx <pedrormx56@gmail.com>
2020-10-23 23:55:25 -0300

    musicaapi

--- app.js
+++ app.js
@@ -3,5 +3,10 @@ async function letraAPI(){
 	const musica = document.getElementById("musica").value
 	const response = await fetch("https://api.vagalume.com.br/search.php?apikey=660a4395f992ff67786584e238f501aa&art="+cantor+"&mus="+musica)
 	const json = await response.json()
+	try{
 	document.getElementById("letra").innerHTML = json.mus[0].text
+	}
+	catch(err){
+	document.getElementById("erro").innerHTML = "Erro ao encontar a letra"
+}
 }
